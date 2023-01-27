ed6331d5abb6214d5eb315191d82f17d34419f41
Ahmed Sameh <ahmeds14@protonmail.ch>
2023-01-27 20:24:57 +0200

    "form log in "

--- js/app.js
+++ js/app.js
@@ -1,66 +1,57 @@
 // Start Header Functions
 // When the user scrolls the page, Fixed Navbar with position sticky
-window.onscroll = function ()
-{
+window.onscroll = function () {
 	myFunction();
 };
 
 // Get the navbar
-var navbar = document.getElementsByClassName( "navbar" )[ 0 ];
+var navbar = document.getElementsByClassName("navbar")[0];
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position.
 // Remove "sticky" when you leave the scroll position
-function myFunction ()
-{
-	if ( window.pageYOffset >= sticky )
-	{
-		navbar.classList.add( "sticky" );
-	} else
-	{
-		navbar.classList.remove( "sticky" );
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
 	}
 }
 // Change Border Input Search
-function ChangeBorder ()
-{
-	var element = document.getElementById( "q" );
+function ChangeBorder() {
+	var element = document.getElementById("q");
 	element.style.borderColor = "gray";
 }
 // End Header Functions
 
 // start slider
-const sliderContainer = document.getElementById( "slider-container" );
-const slider = document.getElementById( "slider" );
+const sliderContainer = document.getElementById("slider-container");
+const slider = document.getElementById("slider");
 
 let count = 0;
 let counter = 0;
 let step = 0;
 
-function StartSlider ()
-{
+function StartSlider() {
 	count = slider.childElementCount;
 	step = sliderContainer.clientWidth;
-	slider.style.width = `${ 100 * count }%`;
+	slider.style.width = `${100 * count}%`;
 }
 StartSlider();
 
-function next ()
-{
-	if ( counter < count - 1 ) counter++;
+function next() {
+	if (counter < count - 1) counter++;
 	move();
 }
 
-function prev ()
-{
-	if ( counter > 0 ) counter--;
+function prev() {
+	if (counter > 0) counter--;
 	move();
 }
-function move ()
-{
-	slider.style.transform = `translateX(${ -step * counter }px)`;
+function move() {
+	slider.style.transform = `translateX(${-step * counter}px)`;
 	step = 942;
 }
 
